4f35d4c993dca97124e448d4ebd300e9a16040ab
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-10-07 13:54:40 +0600

    bootstrap implemeted

--- Clips/disease/disease.pl
+++ Clips/disease/disease.pl
@@ -158,11 +158,41 @@ foreach (@output)
 ###################
 
 print $q->header;
+
+
+print "<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
+
+    <title>Disease Analyzer</title>
+  </head>
+  <body>
+  
+  <div class='container'>
+
+
+  
+  ";
                     
-print $q->start_html('disease'); 
-      
-#<noscript>Your browser does not support JavaScript!</noscript>
+print $q->start_html('disease');
+print "
+</div>
+<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'>
 print $q->noscript('Your browser does not support JavaScript!');
+</script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>
+  </body>
+</html>
+      ";
+#<noscript>Your browser does not support JavaScript!</noscript>
+
 
 ############
 # The Form #
@@ -185,7 +215,7 @@ if ($info{variable})
                     -override => 1);
   }
 
-print $q->start_table({-border=>0,-cellpadding=>'10',-width=>'400'});
+print $q->start_table({-border=>0});
 
 print $q->Tr({-align=>'CENTER',-valign=>'MIDDLE',-height => '50'},
              $q->td($q->h2($info{autoDemoLabel})));
