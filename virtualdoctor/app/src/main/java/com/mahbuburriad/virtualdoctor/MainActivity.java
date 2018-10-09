98f78f8ca89307f2e76e459a796673ed1c6a46b6
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-10-10 03:03:24 +0600

    signin information added from firebase

--- virtualdoctor/app/src/main/java/com/mahbuburriad/virtualdoctor/MainActivity.java
+++ virtualdoctor/app/src/main/java/com/mahbuburriad/virtualdoctor/MainActivity.java
@@ -1,13 +1,38 @@
 package com.mahbuburriad.virtualdoctor;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/riad.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+
     }
 }
---
+++ virtualdoctor/app/src/main/java/com/mahbuburriad/virtualdoctor/Model/user.java
@@ -0,0 +1,9 @@
+package com.mahbuburriad.virtualdoctor.Model;
+
+/**
+ * Created by mahbu on 10-Oct-18.
+ */
+
+public class user {
+    
+}
