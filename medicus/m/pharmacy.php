d9a3f62f7267b3dad56ef346d572dc448b515112
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-10-21 00:53:36 +0600

    search option added in details.php and pharmmacy.php

--- medicus/m/pharmacy.php
+++ medicus/m/pharmacy.php
@@ -22,21 +22,24 @@ include("../assets/function/function.php");
         <link rel="stylesheet" type="text/css" href="styles/framework-store.css">
         <div id="menu-hider"></div>
         <div id="menu-list" data-selected="menu-pages" data-load="menu-list.php" data-height="415" class="menu-box menu-load menu-bottom"></div>
-        <div id="menu-demo" data-load="menu-demo.php" data-height="210" class="menu-box menu-load menu-bottom"></div>
+    
+        <div id="menu-find" data-load="menu-find.php" data-height="420" class="menu-box menu-load menu-bottom"></div>
         <div class="header header-scroll-effect">
             <div class="header-line-1 header-hidden header-logo-app">
                 <a href="#" class="back-button header-logo-title">Back to Store</a>
                 <a href="#" class="back-button header-icon header-icon-1"><i class="fa fa-angle-left"></i></a>
                 <a href="#" data-menu="menu-cart" class="header-icon header-icon-2"><i class="fa fa-shopping-bag"></i></a>
+                <a href="#" data-menu="menu-find" class="header-icon header-icon-3"><i class="fa fa-search"></i></a>
                 <a href="#" data-menu="menu-list" class="header-icon header-icon-4"><i class="fa fa-bars"></i></a>
-                <a href="#" data-menu="menu-demo" class="header-icon header-icon-3"><i class="fa fa-cog"></i></a>
+        
             </div>
             <div class="header-line-2 header-scroll-effect">
                 <a href="#" class="header-pretitle header-date color-highlight"></a>
                 <a href="#" class="header-title">Pharmacy</a>
                 <a href="#" data-menu="menu-list" class="header-icon header-icon-1"><i class="fa fa-bars"></i></a>
                 <a href="#" data-menu="menu-cart" class="header-icon header-icon-3"><i class="fa fa-shopping-bag"></i></a>
-                <a href="#" data-menu="menu-demo" class="header-icon header-icon-2"><i class="fa fa-cog"></i></a>
+                 <a href="#" data-menu="menu-find" class="header-icon header-icon-2"><i class="fa fa-search"></i></a>
+             
             </div>
         </div>
 
