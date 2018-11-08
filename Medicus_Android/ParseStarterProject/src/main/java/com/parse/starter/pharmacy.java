4c1e4626b98556f897d167552ce2affcaa0cbe0c
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-11-09 02:12:32 +0600

    bugs updated for javascipt support

--- Medicus_Android/ParseStarterProject/src/main/java/com/parse/starter/pharmacy.java
+++ Medicus_Android/ParseStarterProject/src/main/java/com/parse/starter/pharmacy.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +15,7 @@ import android.webkit.WebViewClient;
 public class pharmacy extends AppCompatActivity {
     private WebView webView;
     private SwipeRefreshLayout swipe;
+    private WebChromeClient webChromeClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +58,25 @@ public class pharmacy extends AppCompatActivity {
     public void loadWeb(){
         webView = (WebView) findViewById(R.id.webViewId);
 
+
+
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAppCacheEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
         swipe.setRefreshing(true);
+
         webView.loadUrl("http://medicus.ml/mobile");
+
+
         webView.setWebViewClient(new WebViewClient(){
             public void onReceivedError(WebView view, int errorcode, String description, String failingUrl){
                 webView.loadUrl("file:///android_asset/www/index.html");
@@ -69,6 +85,8 @@ public class pharmacy extends AppCompatActivity {
                 swipe.setRefreshing(false);
             }
         });
+
+
 
     }
 
